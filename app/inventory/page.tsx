8f311fe11f412f870dd265fda9c77d3827868a61
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-05-05 20:57:52 -0400

    fixed some type issues and added useCallback

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,31 +1,91 @@
 "use client";
 
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+
+interface InventoryItem {
+  id: number;
+  species: {
+    id: number;
+    colorCat: {
+      id: number;
+      name: string;
+    };
+    species: string;
+    domestic: boolean;
+    hardwood: boolean;
+    density: number;
+    origin: string;
+    appearance: string;
+    characteristics: string;
+    image: string;
+  };
+  format: {
+    id: number;
+    name: string;
+  };
+  entry_date: string;
+  quantity: number;
+  length: number;
+  width: number;
+  thickness: number;
+  totalBF: number;
+  notes: string | null;
+  user: number;
+}
 
 const Inventory = () => {
-  const [inventory, setInventory] = useState([]);
+  const [inventory, setInventory] = useState<InventoryItem[]>([]);
 
   const fetchInventory = async () => {
-    const response = await fetch("http://localhost:8000/inventory", {
+    const response: Response = await fetch("http://localhost:8000/inventory", {
       headers: {
-        Authorization: 'Token 954e988f90c0b292139fbcad13a4ceebd659c9f0',
+        Authorization: "Token 954e988f90c0b292139fbcad13a4ceebd659c9f0",
       },
     });
     return await response.json();
   };
 
-  const fetchAndSetInventory = async () => {
-    const inventoryArray = await fetchInventory();
+  const fetchAndSetInventory = useCallback(async () => {
+    const inventoryArray: InventoryItem[] = await fetchInventory();
     const alphinventory = inventoryArray
       .slice()
-      .sort((a: any, b: any) => a.name.localeCompare(b.name));
+      .sort((a: InventoryItem, b: InventoryItem) =>
+        a.species.species.localeCompare(b.species.species)
+      );
     setInventory(alphinventory);
-  };
+  }, []);
 
   useEffect(() => {
     fetchAndSetInventory();
-  }, []);
+  }, [fetchAndSetInventory]);
+
+  const displayInventory = () => {
+    if (inventory && inventory.length) {
+      return (
+        <div>
+          {inventory.map((item) => (
+            <tr key={item.id}>
+              <td>{item.quantity}</td>
+              <td>{item.species.species}</td>
+              <td>{item.format.name}</td>
+              <td>
+                {item.length} x {item.width} x {item.thickness}
+              </td>
+              <td>{item.totalBF}</td>
+              <td>{item.notes}</td>
+              <div className="buttons">
+                <button className="edit-btn">Edit</button>
+                <button className="edit-btn">Delete</button>
+              </div>
+            </tr>
+          ))}
+        </div>
+      );
+    } else {
+      return <div>No inventory saved yet :/</div>;
+    }
+  };
 
   return (
     <div className="comp-container flex flex-col items-center">
@@ -43,22 +103,12 @@ const Inventory = () => {
             <th>Format</th>
             <th>Dimensions</th>
             <th>Total Board Ft.</th>
+            <th>Entry Date</th>
             <th>Notes</th>
             <th>Edit</th>
           </tr>
         </thead>
-        <tbody>
-            {(inventory && inventory.length) && }
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-          </tr>
-        </tbody>
+        <tbody>{displayInventory()}</tbody>
       </table>
     </div>
   );
